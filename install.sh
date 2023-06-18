c64dbc00119bd1aa3b48ae157ede23868ea2ac38
Hiyabye <hiyabye0465@gmail.com>
2023-06-18 23:48:12 +0900

    Support btrfs

--- install.sh
+++ install.sh
@@ -5,10 +5,7 @@ set -Eeuo pipefail
 NOFORMAT='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 
 # Part 1: Pre-installation
@@ -42,7 +39,7 @@ timedatectl set-ntp true
 echo
 
 # Identify the target disk(s)
-devices=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+devices=$(lsblk -dplnx -o name | grep -Ev "boot|rpmb|loop" | tac)
 echo -e "${BLUE}Available disks:${NOFORMAT}"
 echo
 echo "$devices"
@@ -73,11 +70,11 @@ if [ $uefi -eq 1 ]; then
   parted -s "$device" mklabel gpt \
     mkpart ESP fat32 1MiB 513MiB \
     set 1 boot on \
-    mkpart primary ext4 513MiB 100%
+    mkpart primary btrfs 513MiB 100%
 else
   # For BIOS system, create a root partition only
   parted -s "$device" mklabel msdos \
-    mkpart primary ext4 1MiB 100%
+    mkpart primary btrfs 1MiB 100%
 fi
 echo
 
@@ -85,26 +82,54 @@ echo
 echo -e "${BLUE}Formatting partitions...${NOFORMAT}"
 echo
 if [ $uefi -eq 1 ]; then
-  # For UEFI system, format the ESP as fat32 and the root partition as ext4
+  # For UEFI system, format the ESP as fat32 and the root partition as btrfs
   mkfs.vfat -F 32 "${device}1"
-  mkfs.ext4 "${device}2"
+  mkfs.btrfs "${device}2"
 else
-  # For BIOS system, format the root partition as ext4
-  mkfs.ext4 "${device}1"
+  # For BIOS system, format the root partition as btrfs
+  mkfs.btrfs "${device}1"
 fi
 echo
+
+# Create the Btrfs subvolumes
+echo -e "${BLUE}Creating Btrfs subvolumes...${NOFORMAT}"
+echo
+if [ $uefi -eq 1 ]; then
+  # For UEFI system, create subvolumes for root, home, and snapshots
+  mount "${device}2" /mnt
+  btrfs subvolume create /mnt/@
+  btrfs subvolume create /mnt/@home
+  btrfs subvolume create /mnt/@var
+  btrfs subvolume create /mnt/@snapshots
+  umount /mnt
+else
+  # For BIOS system, create subvolumes for root, home, and snapshots
+  mount "${device}1" /mnt
+  btrfs subvolume create /mnt/@
+  btrfs subvolume create /mnt/@home
+  btrfs subvolume create /mnt/@var
+  btrfs subvolume create /mnt/@snapshots
+  umount /mnt
+fi
 
 # Mount the file systems
 echo -e "${BLUE}Mounting file systems...${NOFORMAT}"
 echo
 if [ $uefi -eq 1 ]; then
   # For UEFI system, mount the root partition to /mnt and the ESP to /mnt/boot
-  mount "${device}2" /mnt
-  mkdir -p /mnt/boot
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@ "${device}2" /mnt
+  mkdir -p /mnt/{boot,home,var,swap,.snapshots}
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@home "${device}2" /mnt/home
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@var "${device}2" /mnt/var
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@snapshots "${device}2" /mnt/.snapshots
   mount "${device}1" /mnt/boot
 else
   # For BIOS system, mount the root partition to /mnt
-  mount "${device}1" /mnt
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@ "${device}1" /mnt
+  mkdir -p /mnt/{home,var,swap,.snapshots}
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@home "${device}1" /mnt/home
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@var "${device}1" /mnt/var
+  mount -o defaults,noatime,nodiratime,compress=zstd,discard=async,space_cache=v2,subvol=@snapshots "${device}1" /mnt/.snapshots
 fi
 echo
 
@@ -114,26 +139,24 @@ echo
 echo -e "${BLUE}Selecting mirrors...${NOFORMAT}"
 echo
 pacman -Sy --noconfirm reflector
-reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 echo
 
 # Enable parallel downloads
 echo -e "${BLUE}Enabling parallel downloads...${NOFORMAT}"
-echo
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 echo
 
 # Install essential packages
 echo -e "${BLUE}Installing essential packages...${NOFORMAT}"
 echo
-pacstrap /mnt base linux linux-firmware base-devel intel-ucode nano btrfs-progs git
+pacstrap -K /mnt base linux linux-firmware base-devel intel-ucode nano dosfstools btrfs-progs git
 echo
 
 # Part 3: Configuration
 
 # Generate Fstab file
 echo -e "${BLUE}Generating fstab file...${NOFORMAT}"
-echo
 genfstab -U /mnt >> /mnt/etc/fstab
 echo
 
@@ -141,14 +164,14 @@ echo
 echo -e "${BLUE}Configuring time zone...${NOFORMAT}"
 echo
 read -p "Enter the time zone (Region/City): " timezone
-arch-chroot /mnt ln -sf "/usr/share/zoneinfo/$timezone" /mnt/etc/localtime
+ln -sf "/mnt/usr/share/zoneinfo/$timezone" /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 echo
 
 # Localization
 echo -e "${BLUE}Configuring localization...${NOFORMAT}"
 echo
-arch-chroot /mnt sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
 echo "KEYMAP=us" >> /mnt/etc/vconsole.conf
@@ -161,7 +184,7 @@ pacman -S --noconfirm networkmanager
 arch-chroot /mnt systemctl enable NetworkManager
 read -p "Enter the hostname: " hostname
 echo "$hostname" > /mnt/etc/hostname
-arch-chroot /mnt cat <<EOF > /mnt/etc/hosts
+cat <<EOF > /mnt/etc/hosts
 127.0.0.1 localhost
 ::1 localhost
 127.0.1.1 $hostname.localdomain $hostname
@@ -171,6 +194,7 @@ echo
 # Initramfs
 echo -e "${BLUE}Creating initramfs...${NOFORMAT}"
 echo
+sed -i 's/^HOOKS.*/HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block filesystems fsck btrfs)/' /mnt/etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -P
 echo
 
@@ -227,7 +251,7 @@ if [ $uefi -eq 1 ]; then
   linux /vmlinuz-linux
   initrd /intel-ucode.img
   initrd /initramfs-linux.img
-  options root=PARTUUID=$(blkid -s PARTUUID -o value "${device}2") rw
+  options root=PARTUUID=$(blkid -s PARTUUID -o value "${device}2") rootflags=subvol=@ rw
 EOF
 else
   arch-chroot /mnt cat <<EOF > /boot/loader/entries/arch.conf
@@ -235,7 +259,7 @@ else
   linux /vmlinuz-linux
   initrd /intel-ucode.img
   initrd /initramfs-linux.img
-  options root=PARTUUID=$(blkid -s PARTUUID -o value "${device}1") rw
+  options root=PARTUUID=$(blkid -s PARTUUID -o value "${device}1") rootflags=subvol=@ rw
 EOF
 fi
 echo
